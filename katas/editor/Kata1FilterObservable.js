4669c7e565fa6052e496a596e477ad2ff317bfeb
Michael Menzel <michael.menzel@senacor.com>
2016-06-18 15:51:08 +0200

    Editor Kata added

---
+++ katas/editor/Kata1FilterObservable.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+	//setup editor
+	var editor = ace.edit("editor");
+	editor.setTheme("ace/theme/twilight");
+	editor.getSession().setMode("ace/mode/javascript");
+
+	function saveInBackend(){
+		console.log("save invoked ...");
+	}
+
+	//register on change event
+	editor.getSession().on('change', function(e) {
+		console.log("Editor Changed!");
+	});
+
+	var observableEventStream = Rx.Observable.fromEvent(editor, "change");
+
+	// 1) Subscribe to the event stream to invoke saveInBackend
+	// 2) Filter the first event to suppress the document loading
+	// 3) Filter the event stream to emit the most recent event within 2 ms
+
+
+	//load Editor value
+	editor.setValue("function foo(items) {\n  var x = \"All this is syntax highlighted\";\n  return x;\n}");
+
+});
+    
